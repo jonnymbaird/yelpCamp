0b9ce5e045c0275917d81fc4c5eb35a8b68deb90
JonnyBaird <jb@jonathanbaird.co.uk>
2018-11-21 11:17:53 +0000

    adding POST method and styling, finished V1

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
 //var request = require("request");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
@@ -24,6 +26,20 @@ app.get("/", function(req, res){
 
 app.get("/campgrounds", function(req, res){
     res.render("campgrounds",{campgrounds: campgrounds});
+});
+
+app.get("/campgrounds/new", function(req, res){
+    res.render("new",{campgrounds: campgrounds});
+});
+
+app.post("/campgrounds", function(req, res){
+    //get data from form and add to campgrounds array
+    var name = req.body.name;
+    var image = req.body.image;
+    var newCampground = {name: name, image: image};
+    campgrounds.push(newCampground);
+    //redirect to campgrounds
+    res.redirect("/campgrounds");
 });
 
 
