cc7cfa24c01d60ef0052de4e022bc276ce09625c
JonnyBaird <jb@jonathanbaird.co.uk>
2018-12-07 17:42:42 +0000

    add user profile page and extra user details to model

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require("express"),
     router = express.Router(),
     User = require("../models/user"),
+    Campground = require("../models/campground"),
     passport = require("passport"),
     middleware = require("../middleware");
     
@@ -21,7 +22,13 @@ router.get("/register", function(req, res) {
 });
 
 router.post("/register", function(req, res) {
-    const newUser = new User({ username: req.body.username });
+    const newUser = new User({ 
+        username: req.body.username, 
+        firstName: req.body.firstName,  
+        lastName: req.body.lastName,
+        email: req.body.email,
+        avatar: req.body.avatar
+    });
     if(req.body.adminCode === "secretcode123"){
         newUser.isAdmin = true;
     }
@@ -53,5 +60,22 @@ router.get("/logout", function(req, res) {
     req.flash("success", "Logged you out");
     res.redirect("back");
 })
+
+// USER PROFILE
+router.get("/users/:id", function(req, res) {
+    User.findById(req.params.id, function(err, foundUser){
+        if(err){
+            req.flash("error", err.message);
+            res.redirect("back");
+        }
+        Campground.find().where("author.id").equals(foundUser._id).exec(function(err, campgrounds){
+            if(err){
+                req.flash("error", err.message);
+                res.redirect("back");
+            }
+            res.render("users/show", {user: foundUser, campgrounds: campgrounds});
+        });
+    });
+});
 
 module.exports = router;
